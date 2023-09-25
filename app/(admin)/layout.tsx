c6017516e13d7405b3d120646b28f3e202a7cbfc
wming <wming217@outlook.com>
2023-09-25 22:44:05 +0800

    feat: add sidebar

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,3 +1,14 @@
+import {
+  LayoutGrid,
+  Library,
+  ListMusic,
+  Mic2,
+  Music2,
+  PlayCircle,
+  Radio,
+  User,
+} from "lucide-react"
+
 import Header from "@/components/Header"
 import Sidebar from "@/components/Sidebar"
 
@@ -10,23 +21,64 @@ export default function AdminLayout({
   //   return <div>hello</div>
   // }
 
-  const playlists = [
-    "Recently Added",
-    "Recently Played",
-    "Top Songs",
-    "Top Albums",
-    "Top Artists",
-    "Logic Discography",
-    "Bedtime Beats",
-    "Feeling Happy",
-    "I miss Y2K Pop",
-    "Runtober",
-    "Mellow Days",
-    "Eminem Essentials",
+  const menu = [
+    {
+      label: "Discover",
+      path: "200000",
+      children: [
+        {
+          label: "Listen Now",
+          path: "/main",
+          icon: <PlayCircle size={16} className='mr-2' />,
+        },
+        {
+          label: "Browse",
+          path: "/2",
+          icon: <LayoutGrid size={16} className='mr-2' />,
+        },
+        {
+          label: "Radio",
+          path: "/3",
+          icon: <Radio size={16} className='mr-2' />,
+        },
+      ],
+    },
+    {
+      label: "Library",
+      path: "4",
+      children: [
+        {
+          label: "Playlists",
+          path: "/4",
+          icon: <ListMusic size={16} className='mr-2' />,
+        },
+        {
+          label: "Songs",
+          path: "/5",
+          icon: <Music2 size={16} className='mr-2' />,
+        },
+        {
+          label: "Made for You",
+          path: "/6",
+          icon: <User size={16} className='mr-2' />,
+        },
+        {
+          label: "Artists",
+          path: "/7",
+          icon: <Mic2 size={16} className='mr-2' />,
+        },
+        {
+          label: "Albums",
+          path: "/8",
+          icon: <Library size={16} className='mr-2' />,
+        },
+      ],
+    },
   ]
+
   return (
     <main className='flex'>
-      <Sidebar playlists={playlists} />
+      <Sidebar items={menu} />
 
       <div className='flex-1'>
         <Header />
--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,168 +1,92 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+import { usePathname } from "next/navigation"
+
 import { cn } from "@/lib/utils"
 
 import { Button } from "../ui/button"
 import { ScrollArea } from "../ui/scroll-area"
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  playlists: string[]
+  items: MenuItem[]
 }
 
-export default function Sidebar({ className, playlists }: SidebarProps) {
+interface MenuItem {
+  label: string
+  path: string
+  icon?: React.ReactElement
+  children?: MenuItem[]
+}
+const playlists = [
+  "Recently Added",
+  "Recently Played",
+  "Top Songs",
+  "Top Albums",
+  "Top Artists",
+  "Logic Discography",
+  "Bedtime Beats",
+  "Feeling Happy",
+  "I miss Y2K Pop",
+  "Runtober",
+  "Mellow Days",
+  "Eminem Essentials",
+]
+export default function Sidebar({ className, items }: SidebarProps) {
+  const pathName = usePathname()
+  const [path, setPath] = useState(pathName)
+
+  const onClick = (val: string) => {
+    setPath(val)
+  }
+  const renderChildren = (items: MenuItem[]) => {
+    return items.map((item) => {
+      if (item.children) {
+        return (
+          <div key={item.label} className='px-3 py-2'>
+            <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>
+              {item.label}
+            </h2>
+            <div className='space-y-1'>
+              {item.children.map((el) => {
+                return (
+                  <Button
+                    key={el.label}
+                    variant={el.path === path ? "secondary" : "ghost"}
+                    className='w-full justify-start'
+                    onClick={() => onClick(el.path)}
+                  >
+                    {el.icon}
+                    {el.label}
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        )
+      } else {
+        return (
+          <div className='space-y-1' key={item.label}>
+            <Button
+              key={item.label}
+              variant={item.path === path ? "secondary" : "ghost"}
+              className='w-full justify-start'
+              onClick={() => onClick(item.path)}
+            >
+              {item.icon}
+              {item.label}
+            </Button>
+          </div>
+        )
+      }
+    })
+  }
+
   return (
-    <div className={cn("pb-12 w-[280px]", className)}>
+    <div className={cn("w-[280px] pb-12", className)}>
       <div className='space-y-4 py-4'>
-        <div className='px-3 py-2'>
-          <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>
-            Discover
-          </h2>
-          <div className='space-y-1'>
-            <Button variant='secondary' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <circle cx='12' cy='12' r='10' />
-                <polygon points='10 8 16 12 10 16 10 8' />
-              </svg>
-              Listen Now
-            </Button>
-            <Button variant='ghost' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <rect width='7' height='7' x='3' y='3' rx='1' />
-                <rect width='7' height='7' x='14' y='3' rx='1' />
-                <rect width='7' height='7' x='14' y='14' rx='1' />
-                <rect width='7' height='7' x='3' y='14' rx='1' />
-              </svg>
-              Browse
-            </Button>
-            <Button variant='ghost' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <path d='M4.9 19.1C1 15.2 1 8.8 4.9 4.9' />
-                <path d='M7.8 16.2c-2.3-2.3-2.3-6.1 0-8.5' />
-                <circle cx='12' cy='12' r='2' />
-                <path d='M16.2 7.8c2.3 2.3 2.3 6.1 0 8.5' />
-                <path d='M19.1 4.9C23 8.8 23 15.1 19.1 19' />
-              </svg>
-              Radio
-            </Button>
-          </div>
-        </div>
-        <div className='px-3 py-2'>
-          <h2 className='mb-2 px-4 text-lg font-semibold tracking-tight'>
-            Library
-          </h2>
-          <div className='space-y-1'>
-            <Button variant='ghost' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <path d='M21 15V6' />
-                <path d='M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z' />
-                <path d='M12 12H3' />
-                <path d='M16 6H3' />
-                <path d='M12 18H3' />
-              </svg>
-              Playlists
-            </Button>
-            <Button variant='ghost' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <circle cx='8' cy='18' r='4' />
-                <path d='M12 18V2l7 4' />
-              </svg>
-              Songs
-            </Button>
-            <Button variant='ghost' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <path d='M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2' />
-                <circle cx='12' cy='7' r='4' />
-              </svg>
-              Made for You
-            </Button>
-            <Button variant='ghost' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <path d='m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12' />
-                <circle cx='17' cy='7' r='5' />
-              </svg>
-              Artists
-            </Button>
-            <Button variant='ghost' className='w-full justify-start'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='mr-2 h-4 w-4'
-              >
-                <path d='m16 6 4 14' />
-                <path d='M12 6v14' />
-                <path d='M8 8v12' />
-                <path d='M4 4v16' />
-              </svg>
-              Albums
-            </Button>
-          </div>
-        </div>
+        {renderChildren(items)}
+
         <div className='py-2'>
           <h2 className='relative px-7 text-lg font-semibold tracking-tight'>
             Playlists
