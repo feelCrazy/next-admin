a2c4b74f71f37ce8dbd4806e7a74ac7e57d85161
wming <wming217@outlook.com>
2023-09-29 20:10:08 +0800

    feat: add

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+import { useEffect, useRef, useState } from "react"
+
+import { cn } from "@/lib/utils"
+
 import CommandMenu from "../CommandMenu"
 import Setting from "../Setting"
 import SwtichLanguage from "../SwitchLanguage"
@@ -7,19 +13,45 @@ import UserMenu from "../UserMenu"
 interface Props {}
 
 export default function Header({}: Props) {
-  return (
-    <div
-      className='sticky top-0 z-50 flex h-14 items-center justify-between
-     border-b border-cyan-300 bg-background px-8'
-    >
-      <CommandMenu />
+  const navRef = useRef(null)
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        setShow(!entries[0].isIntersecting)
+      },
+      {
+        root: null,
+        rootMargin: `10px 0px`,
+        threshold: 0,
+      },
+    )
 
-      <div className='flex items-center gap-4'>
-        <SwtichLanguage />
-        <SwitchMode />
-        <Setting />
-        <UserMenu />
+    intersectionObserver.observe(navRef.current!)
+
+    return () => intersectionObserver.disconnect()
+  }, [])
+
+  return (
+    <>
+      <div ref={navRef}></div>
+      <div
+        className={cn(
+          "sticky top-0 z-50  flex h-16 items-center justify-between border-b bg-background px-8",
+          {
+            "h-12": show,
+          },
+        )}
+      >
+        <CommandMenu />
+
+        <div className='flex items-center gap-4'>
+          <SwtichLanguage />
+          <SwitchMode />
+          <Setting />
+          <UserMenu />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
